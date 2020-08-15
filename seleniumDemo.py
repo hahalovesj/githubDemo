f22ace860d5e9771562bd8e922145f70646e1d4d
hahalovesj <shenjing8412@sina.com>
2020-08-15 15:58:15 +0800

    demo
    demo11

---
+++ seleniumDemo.py
@@ -0,0 +1,14 @@
+from selenium import webdriver # 导入webdriver包
+
+driver = webdriver.Chrome()
+
+driver.maximize_window() # 最大化浏览器 
+
+
+driver.get("https://twww.lizihang.com/") # 通过get()方法，打开一个url站点
+
+
+ershoufang = driver.find_element_by_css_selector("[autoid='auto_20qlf3di']")
+
+ershoufang.click()
+print("---点击二手房 频道----")
